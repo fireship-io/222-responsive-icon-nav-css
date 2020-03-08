335ffee4e76ef465b099c13c74423d8e2b970220
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-03-08 05:17:04 -0700

    Fix bug where theme was undefined at first use (again)

--- public/theme.js
+++ public/theme.js
@@ -4,7 +4,10 @@ const themeMap = {
   solar: "dark"
 };
 
-const theme = localStorage.getItem('theme') || Object.keys(themeMap)[0];
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
 const bodyClass = document.body.classList;
 bodyClass.add(theme);
 
